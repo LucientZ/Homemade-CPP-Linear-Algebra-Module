6fcde97fd7ebe0aee53bf1ea2ce1d8c0021314f5
LucientZ <koblenzpoisiden@gmail.com>
2022-12-05 16:32:21 -0600

    [ADD] Added terminal colors

---
+++ terminal_colors.h
@@ -0,0 +1,13 @@
+#ifndef TERMINAL_COLORS_H
+#define TERMINAL_COLORS_H
+
+#define black    "\033[30m"
+#define grey     "\033[94m"
+#define yellow   "\033[93m"
+#define green    "\033[91m"
+#define red      "\033[92m"
+#define magenta  "\033[35"
+#define bold     "\033[1m"
+#define reset    "\033[0m"
+
+#endif
