c60d410f4f443cdc6d0cd4d57d3c0a8aa23ec0bd
LucientZ <koblenzpoisiden@gmail.com>
2022-12-10 21:13:04 -0600

    [ADD] Added basic framework for tests

--- tests.cpp
+++ tests.cpp
@@ -1,16 +1,37 @@
 #include "tests.h"
 #include <iostream>
-
-
 using namespace std; // Temporary using statement to be replaced.
 
+
+namespace {
+    bool test_passed = true;
+}
+
+
+bool test_fraction(){
+
+    FINISH_TEST;
+}
+
+
+bool true_returner(){
+    FINISH_TEST
+}
+
+bool false_returner(){
+    test_passed = false;
+    FINISH_TEST
+}
 
 
 
 int main(){
+    unsigned passes = 0, fails = 0, skips = 0;
+
     cout << "Begin Tests" << "\n";
+    test(true_returner);
+    test(false_returner);
+    skip(false_returner)
 
-
-
-    cout << flush;
+    cout << '\n' << red << "Fails: " << fails << '\n' << green << "Passes: " << passes << '\n' << yellow << "Skips: " << skips << '\n' << reset << flush;
 }
--- tests.h
+++ tests.h
@@ -1,8 +1,31 @@
 #ifndef TESTS_H
 #define TESTS_H
 
+#include <iostream>
+#include "terminal_colors.h"
+
 #define here cout << __FUNCTION__ << " | " << __LINE__ << endl;
 
+#define expect(expression)\
+if(!expression){\
+    std::cout << red << "[FAILURE] " << reset << "Unexpected value: " << here << "Expected: " << #expression << std::endl;\
+}
+
+
+#define test(func)\
+if (func()) {std::cout << green << "[SUCCESS] " << #func << '\n'; passes++;}\
+else {std::cout << red << "[FAILURE] " << #func << '\n'; fails++;}\
+std::cout << reset;
+
+#define skip(func) std::cout << yellow << "[SKIP] " << #func << reset << '\n';
+
+
+#define is ==
+#define is_not !=
+
+#define FINISH_TEST bool current_test_passed = test_passed;\
+test_passed = true;\
+return current_test_passed;
 
 
 #endif
